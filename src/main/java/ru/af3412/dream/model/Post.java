51709404a9684aa9d9010527a1fc2d2ea1946017
Alexander Filatov <alex.filatov@sparox.ru>
2020-11-15 19:10:14 +0300

    Scriplet. Динамическая таблица. [#276520]

---
+++ src/main/java/ru/af3412/dream/model/Post.java
@@ -0,0 +1,56 @@
+package ru.af3412.dream.model;
+
+import java.util.Objects;
+
+public class Post {
+
+    private final int id;
+    private final String name;
+    private final String description;
+    private final String created;
+
+    public Post(int id, String name, String description, String created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        if (id != post.id) return false;
+        if (!Objects.equals(name, post.name)) return false;
+        if (!Objects.equals(description, post.description)) return false;
+        return Objects.equals(created, post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/ru/af3412/dream/store/Store.java
@@ -0,0 +1,28 @@
+package ru.af3412.dream.store;
+
+import ru.af3412.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Store {
+
+    private static final Store INST = new Store();
+
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job", "desc 1", "2020 11 15"));
+        posts.put(2, new Post(2, "Middle Java Job", "desc 1", "2020 11 15"));
+        posts.put(3, new Post(3, "Senior Java Job", "desc 1", "2020 11 15"));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
