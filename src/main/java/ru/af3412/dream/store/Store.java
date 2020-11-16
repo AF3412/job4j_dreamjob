819473dd3438ccd6fa5247c7673c496a2d1b8ed2
Alexander Filatov <alex.filatov@sparox.ru>
2020-11-16 10:04:52 +0300

    4. candicates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/af3412/dream/store/Store.java
+++ src/main/java/ru/af3412/dream/store/Store.java
@@ -1,5 +1,6 @@
 package ru.af3412.dream.store;
 
+import ru.af3412.dream.model.Candidate;
 import ru.af3412.dream.model.Post;
 
 import java.util.Collection;
@@ -10,19 +11,27 @@ public class Store {
 
     private static final Store INST = new Store();
 
-    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "desc 1", "2020 11 15"));
-        posts.put(2, new Post(2, "Middle Java Job", "desc 1", "2020 11 15"));
-        posts.put(3, new Post(3, "Senior Java Job", "desc 1", "2020 11 15"));
+        posts.put(2, new Post(2, "Middle Java Job", "desc 2", "2020 11 15"));
+        posts.put(3, new Post(3, "Senior Java Job", "desc 3", "2020 11 15"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
